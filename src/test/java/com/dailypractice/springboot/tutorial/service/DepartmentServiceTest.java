ab5dc5883319f991e210da1d87f0668e30440ba7
kshithijakappa <kshithijakappa@gmail.com>
2022-05-12 22:54:17 -0400

    updated db from h2 to mysql

---
+++ src/test/java/com/dailypractice/springboot/tutorial/service/DepartmentServiceTest.java
@@ -0,0 +1,28 @@
+package com.dailypractice.springboot.tutorial.service;
+
+import com.dailypractice.springboot.tutorial.entity.Department;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void whenValidDepartmentName_ThenDeoartmentShouldFound(){
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+
+        assertEquals(departmentName, found.getDepartmentName() );
+    }
+}
